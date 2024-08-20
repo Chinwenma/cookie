a5af829b3ff4008766c45e21665ca251daf4fc58
Chinwenma <okoriechinwe1327@gmail.com>
2024-08-20 15:26:40 -0800

    k

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,27 @@
-export default function ProfilePage() {
+import { getSession } from "@/actions";
+import { redirect } from "next/navigation";
+
+const ProfilePage = async () => {
+  const session = await getSession();
+
+  if (!session.isLoggedIn) {
     return (
-      <main>
-        <div>
-          <h1>Hello user</h1>
-        </div>
-      </main>
+redirect("/login")
+        
+      
     );
+    
   }
+
+  return (
+    <div className="profile">
+      <h1>Hello user</h1>
+      <p>
+        welcome,
+        <b>{session.username}</b>
+      </p>
+      <span>You are a <b>{session.isPro ? "Pro" : "Free"} user</b></span>
+    </div>
+  );
+};
+export default ProfilePage;
